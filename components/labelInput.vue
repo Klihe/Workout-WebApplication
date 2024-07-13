dd4d15f7fb84c06c146d12865112737109a69938
Klihe <stepan.birke@gmail.com>
2024-07-13 16:55:20 +0200

    feat: better labelInput

--- components/labelInput.vue
+++ components/labelInput.vue
@@ -1,26 +1,64 @@
 <template>
-    <div>
-      <label for="inputField">{{ label }}</label>
-      <input type="text" id="inputField" v-model="inputValue" />
+  <div class="all">
+    <div class="label">
+      <label class="label-text">{{ nameOfExercise }}</label>
+      <label class="label-text">series: {{ numberOfSeries }}</label>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'LabelInput',
-    props: {
-      label: {
-        type: String,
-        required: true
-      }
-    },
-    data() {
-      return {
-        inputValue: ''
-      };
-    }
-  };
-  </script>
-  
-  <style>
-  </style>
+    <div v-for="index in numberOfSeries" :key="'input' + index" class="input">
+      <input v-if="weight" type="number" v-model="weightSeriesNumbers[index]" placeholder="weight [kg]" class="input-field" />
+      <input type="number" v-model="repSeriesNumbers[index]" placeholder="reps" class="input-field" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LabelInput',
+  props: {
+    nameOfExercise: String,
+    numberOfSeries: Number,
+    weight: Boolean,
+  },
+  data() {
+    return {
+      repSeriesNumbers: [this.numberOfSeries],
+      weightSeriesNumbers: [this.numberOfSeries],
+    };
+  },
+};
+</script>
+
+<style>
+.all {
+  display: flex;
+  justify-content: center;
+}
+
+.label {
+  display: flex;
+  flex-direction: column;
+  font-family: 'Arial', sans-serif;
+  font-size: 1.2em;
+  margin-top: 10px;
+}
+
+.label-text {
+  margin-bottom: 5px;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 10px;
+}
+
+.input-field {
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-left: 2px;
+  margin-right: 2px;
+  margin-bottom: 2px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,37 @@
 <template>
-    <div id="app">
-      <LabelInput v-for="index in inputs.length" :key="index" :label="'Enter your text: '+ (index)" />
-      <button @click="buttonClicked">add new</button>
-    </div>
-  </template>
-  
-  <script>
-  import LabelInput from '../components/labelInput.vue';
-  
-  export default {
-    name: 'App',
-    components: {
-      LabelInput
-    },
-    data() {
-      return {
-        inputs: []
-      };
-    },
-    methods: {
-      buttonClicked() {
-        this.inputs.push({});
-      }
-    }
-  };
-  </script>
-  
-  <style>
-  </style>
+  <div id="app">
+    <h1>Workout plan</h1>
+    <LabelInput 
+      nameOfExercise="Push-ups" 
+      :numberOfSeries="3" 
+      :weight="true" 
+    />
+  </div>
+</template>
+n
+<script>
+import LabelInput from '../components/labelInput.vue';
+
+export default {
+  name: 'TestPage',
+  components: {
+    LabelInput,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: #FFF5E1;
+}
+
+h1 {
+  font-size: 2em;
+  color: #333;
+  text-align: center;
+  margin-top: 20px;
+  font-family: 'Arial', sans-serif;
+}
+</style>
+
   
