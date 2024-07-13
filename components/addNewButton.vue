96e57e4cdd1b96c72dcbfbbd737771f73b682b57
Klihe <stepan.birke@gmail.com>
2024-07-13 18:01:00 +0200

    feat: addNewButton

---
+++ components/addNewButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="all">
+        <div class="inputs">
+            <input type="text" v-model="name" placeholder="Name of exercise" class="input-field" />
+            <input type="number" v-model="series" placeholder="Number of series" class="input-field" />
+            <input type="checkbox" v-model="weight" class="input-field" />
+        </div>
+        <div class="button">
+            <label>Weight</label>
+            <button @click="addNewExercise" class="add-button">Add new exercise</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddNewButton',
+    data() {
+        return {
+            exercises: [],
+            name: '',
+            series: '',
+            weight: false
+        };
+    },
+    methods: {
+        addNewExercise() {
+            if (this.name === '' || this.series === '' || this.series < 1) {
+                return;
+            }
+            
+            this.exercises.push({
+                name: this.name,
+                series: this.series,
+                weight: this.weight
+            });
+            this.$emit('add:exercises', this.exercises);
+
+            // Clear the inputs
+            this.name = '';
+            this.series = '';
+            this.weight = false;
+        }
+    }
+};
+</script>
+
+<style>
+.all{
+    display: flex;
+    justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,36 @@
 <template>
   <div id="app">
     <h1>Workout plan</h1>
-    <LabelInput 
-      nameOfExercise="Push-ups" 
-      :numberOfSeries="3" 
-      :weight="true" 
+    <addNewButton @add:exercises="addExercise"/>
+    <LabelInput
+      v-for="(exercise, index) in exercisesList"
+      :key="index"
+      :nameOfExercise="exercise.name"
+      :numberOfSeries="exercise.series"
+      :weight="exercise.weight"
     />
   </div>
 </template>
 n
 <script>
 import LabelInput from '../components/labelInput.vue';
+import AddNewButton from '../components/addNewButton.vue';
 
 export default {
   name: 'TestPage',
   components: {
     LabelInput,
+    AddNewButton,
+  },
+  data() {
+    return {
+      exercisesList: [],
+    };
+  },
+  methods: {
+    addExercise(exercise) {
+      this.exercisesList = exercise;
+    },
   },
 };
 </script>
