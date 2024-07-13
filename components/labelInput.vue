d679da1524e7d865c7d57fc709747e543f8cf67f
Klihe <stepan.birke@gmail.com>
2024-07-13 15:00:21 +0200

    feat: labelInput & addNewButton

---
+++ components/labelInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <label for="inputField">{{ label }}</label>
+      <input type="text" id="inputField" v-model="inputValue" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LabelInput',
+    props: {
+      label: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        inputValue: ''
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+      <LabelInput v-for="index in inputs.length" :key="index" :label="'Enter your text: '+ (index)" />
+      <button @click="buttonClicked">add new</button>
+    </div>
+  </template>
+  
+  <script>
+  import LabelInput from '../components/labelInput.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      LabelInput
+    },
+    data() {
+      return {
+        inputs: []
+      };
+    },
+    methods: {
+      buttonClicked() {
+        this.inputs.push({});
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
+  
